cbd111f6ca702a6153baeb39fe551a45e433ade0
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-06-02 20:38:01 +0700

    try PySimpleGUI to create a pop-up to enter a file..

--- DiceRollingSimulator.py
+++ DiceRollingSimulator.py
@@ -1,4 +1,9 @@
 import random
-
+import PySimpleGUI as sg
 print("This is Dice Rolling Simulator for You")
 print("Your point is", random.randint(1, 6))
+
+import PySimpleGUI as sg
+
+filename = sg.popup_get_file('Enter the file you wish to process')
+sg.popup('You entered', filename)
