9c34663bf67a5942399e8fff50d8f2f83f92595e
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-06-03 20:27:19 +0700

    Mình bắt đầu học PyQt

--- DiceRollingSimulator.py
+++ DiceRollingSimulator.py
@@ -1,9 +1,2 @@
 import random
-import PySimpleGUI as sg
-print("This is Dice Rolling Simulator for You")
-print("Your point is", random.randint(1, 6))
-
-import PySimpleGUI as sg
-
-filename = sg.popup_get_file('Enter the file you wish to process')
-sg.popup('You entered', filename)
+# Start to learn PyQt5
